b6d4d588c7be87d95fe60b72ca638218079300c5
Stephan Geulette <s.geulette@imio.be>
2021-05-18 15:54:28 +0200

    Workaround to import error

--- src/imio/zamqp/dms/tests/test_incoming_email.py
+++ src/imio/zamqp/dms/tests/test_incoming_email.py
@@ -2,7 +2,6 @@
 
 from imio.dataexchange.core.dms import IncomingEmail as CoreIncomingEmail
 from imio.dms.mail.testing import DMSMAIL_INTEGRATION_TESTING
-# from imio.zamqp.dms.consumer import IncomingEmail
 from imio.zamqp.dms.testing import create_fake_message
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
@@ -23,11 +22,12 @@ class TestDmsfile(unittest.TestCase):
         self.omf = self.portal['outgoing-mail']
 
     def test_IncomingEmail_base(self):
+        from imio.zamqp.dms.consumer import IncomingEmail  # import later to avoid core config error
         params = {'external_id': u'01Z999900000002', 'client_id': u'019999', 'type': u'EMAIL_E', 'version': 1,
                   'date': datetime.datetime(2021, 5, 18), 'update_date': None, 'user': u'testuser', 'file_md5': u'',
                   'file_metadata': {u'creator': u'scanner', u'scan_hour': u'13:16:29', u'scan_date': u'2021-05-18',
                                     u'filemd5': u'', u'filename': u'01Z999900000001.tar', u'pc': u'pc-scan01',
                                     u'user': u'testuser', u'filesize': 0}}
         msg = create_fake_message(CoreIncomingEmail, params)
-#        ie = IncomingEmail('incoming-mail', 'dmsincoming_email', msg)
+        ie = IncomingEmail('incoming-mail', 'dmsincoming_email', msg)
         self.assertTrue(True)
