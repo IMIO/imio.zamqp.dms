053217c7b135aa809656eef0e3bd136d14668b77
Stephan Geulette <s.geulette@imio.be>
2021-05-19 12:16:52 +0200

    Made first test is working

--- src/imio/zamqp/dms/testing.py
+++ src/imio/zamqp/dms/testing.py
@@ -4,6 +4,10 @@ from imio.dataexchange.core.document import Document
 from imio.dms.mail.utils import Dummy
 
 import cPickle
+import json
+import os
+import tarfile
+import tempfile
 
 
 def create_fake_message(klass, dic):
@@ -11,3 +15,25 @@ def create_fake_message(klass, dic):
     doc = Document(**dic)
     inst = klass(doc)
     return Dummy(body=cPickle.dumps(inst))
+
+
+def create_tarfile(name, content):
+    """Create a tar file with an empty pdf file and the content."""
+    tdir = tempfile.mkdtemp()
+    f = open(os.path.join(tdir, 'email.pdf'), 'w')
+    f.close()
+    f = open(os.path.join(tdir, 'metadata.json'), 'w')
+    f.write(content)
+    f.close()
+    f = tarfile.open(os.path.join(tdir, name), 'w')
+    f.add(os.path.join(tdir, 'email.pdf'), arcname='email.pdf')
+    f.add(os.path.join(tdir, 'metadata.json'), arcname='metadata.json')
+    f.close()
+    return open(os.path.join(tdir, name), 'rb')
+
+
+def store_fake_content(klass, params, metadata):
+    """"""
+    fh = create_tarfile(params['file_metadata']['filename'], json.dumps(metadata))
+    setattr(klass, 'file_content', fh.read())
+    fh.close()
--- src/imio/zamqp/dms/tests/test_incoming_email.py
+++ src/imio/zamqp/dms/tests/test_incoming_email.py
@@ -3,8 +3,10 @@
 from imio.dataexchange.core.dms import IncomingEmail as CoreIncomingEmail
 from imio.dms.mail.testing import DMSMAIL_INTEGRATION_TESTING
 from imio.zamqp.dms.testing import create_fake_message
+from imio.zamqp.dms.testing import store_fake_content
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
+from plone import api
 
 import datetime
 import unittest
@@ -23,11 +25,26 @@ class TestDmsfile(unittest.TestCase):
 
     def test_IncomingEmail_base(self):
         from imio.zamqp.dms.consumer import IncomingEmail  # import later to avoid core config error
-        params = {'external_id': u'01Z999900000002', 'client_id': u'019999', 'type': u'EMAIL_E', 'version': 1,
-                  'date': datetime.datetime(2021, 5, 18), 'update_date': None, 'user': u'testuser', 'file_md5': u'',
-                  'file_metadata': {u'creator': u'scanner', u'scan_hour': u'13:16:29', u'scan_date': u'2021-05-18',
-                                    u'filemd5': u'', u'filename': u'01Z999900000001.tar', u'pc': u'pc-scan01',
-                                    u'user': u'testuser', u'filesize': 0}}
+        params = {
+            'external_id': u'01Z999900000002', 'client_id': u'019999', 'type': u'EMAIL_E', 'version': 1,
+            'date': datetime.datetime(2021, 5, 18), 'update_date': None, 'user': u'testuser', 'file_md5': u'',
+            'file_metadata': {u'creator': u'scanner', u'scan_hour': u'13:16:29', u'scan_date': u'2021-05-18',
+                              u'filemd5': u'', u'filename': u'01Z999900000001.tar', u'pc': u'pc-scan01',
+                              u'user': u'testuser', u'filesize': 0}
+        }
+        metadata = {
+            'From': [['Dexter Morgan', 'dexter.morgan@mpd.am']], 'To': [['', 'debra.morgan@mpd.am']], 'Cc': [],
+            'Subject': 'Bloodstain pattern analysis', 'Origin': 'Agent forward',
+            'Agent': [['Vince Masuka', 'vince.masuka@mpd.am']]
+        }
+        store_fake_content(IncomingEmail, params, metadata)
         msg = create_fake_message(CoreIncomingEmail, params)
         ie = IncomingEmail('incoming-mail', 'dmsincoming_email', msg)
-        self.assertTrue(True)
+        ie.create_or_update()
+        obj = self.pc(portal_type='dmsincoming_email')[0].getObject()
+        self.assertEqual(obj.mail_type, u'email')
+        self.assertEqual(obj.original_email_sender, u'"Dexter Morgan" <dexter.morgan@mpd.am>')
+        self.assertIsNone(obj.sender)
+        self.assertIsNone(obj.treating_groups)
+        self.assertIsNone(obj.assigned_user)
+        self.assertEqual(api.content.get_state(obj), 'created')
